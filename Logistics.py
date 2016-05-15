d68515cead5ed22fa0b6a3dfaa162ed4b4532861
shaohuimao <shaohuimao@sohu-inc.com>
2016-05-15 21:40:57 +0800

    modofy

--- Logistics.py
+++ Logistics.py
@@ -1,3 +1,7 @@
+# -*- coding: UTF-8 -*-
+import math
+import Numpy
+
 def loadDataSet():
     dataMat = [];
     labelMat = [];
@@ -7,3 +11,12 @@ def loadDataSet():
         dataMat.append(1.0, float(lineArr[0]), float(lineArr[1]))
         labelMat.append(int(lineArr[2]))
     return dataMat, labelMat
+
+
+def sigmoid(inX):
+    return 1.0 / (1 + exp(-inX))
+
+
+
+def gradAscent(dataMatIn, classLabels):
+    datMatrix
